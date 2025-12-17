cb0993afd1abb3b465a5e561eb644c81072c7ebd
Lemos <gabriellemoscardim@gmail.com>
2025-12-17 18:29:54 -0300

    Endpoint de Registro (Sign-up)

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 from backend.database import get_supabase_client
-from backend.schemas import ResetPasswordRequest, UserLogin 
+from backend.schemas import ResetPasswordRequest, UserLogin, UserSign
 
 # Cria o cliente supabase
 supabase = get_supabase_client()
@@ -28,6 +28,58 @@ def health_check():
         "status": "ok", 
         "message": "API SeccoGeoCarbo rodando 🚀"
     }
+
+# --- ROTA: SIGN-UP (CRIAÇÃO DE CONTA) ---
+@app.post("/auth/signup")
+def signup(user: UserSign):
+    try:
+        #Query para validar a existencia do email cadastrado
+        existing_user = supabase.table("profiles").select("email").eq("email", user.email.lower()).execute()
+        #print(existing_user)
+            
+        #Chamando o método para criar o user no SupaBase
+        response = supabase.auth.sign_up({
+            "email": user.email,
+            "password": user.password,
+            "options": {
+                "data": {
+                    "full_name": user.full_name
+                }
+            }
+        })
+        
+        #Se criar corretamente, retorna o 201 (Created)
+        return {
+            "message": "Usuário criado com sucesso!",
+            "user": {
+                "email": user.email,
+                "full_name": user.full_name
+            }
+        }
+        
+    except Exception as e: 
+        #Printando o erro completo para ver o que ele retorna
+        #print(f"Erro completo: {e}")
+
+        #Se o email já estiver cadastrado
+        if existing_user.data:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Email já cadastrado"
+            )
+
+        #Se for 429 (Too Many Requests)
+        if "security" in str(e).lower() and "seconds" in str(e).lower():
+            raise HTTPException(
+                status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+                detail="Você excedeu o limite de requisições. Tente novamente em alguns segundos."
+            )
+        
+        #Caso seja outro erro, retoanr uma resposta genérica
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Erro ao criar o usuário"
+        )
 
 # --- ROTA: LOGIN ---
 @app.post("/auth/login")
@@ -65,3 +117,4 @@ def reset_password(data: ResetPasswordRequest):
     return{
         "message":"Se o email existir, enviaremos um link para redefinição de senha"
     }
+    
